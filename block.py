b18f9c97452ddc168dea4c4368c8b8ab3a27b6d3
Roy Zheng <roycraft3@gmail.com>
2016-02-27 12:29:49 -0800

    merged site.py into block.py

--- block.py
+++ block.py
@@ -1,7 +1,43 @@
+import thread
+
 from plumbum import local
 from plumbum.cmd import cp, echo, ls
-cp = local["cp"]
+from firebase import firebase
+
 resetHost = cp['/etc/hosts.backup', '/etc/hosts']
+PULL_RATE=2500
+PORT = 8000
+firebase = firebase.FirebaseApplication(
+    'https://torrid-torch-8987.firebaseio.com/',
+    None)
+
+def server():
+    import SimpleHTTPServer
+    import SocketServer
+    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+    httpd = SocketServer.TCPServer(("", PORT), Handler)
+    print "serving at port", PORT
+    httpd.serve_forever()
+
+def pullFromServer():
+    result = firebase.get('/',None)
+    return result.steps
+    
+def generateSite(raw_steps):
+    from jinja2 import Template
+    template = Template(
+        open('template.html', 'w').read()
+        )
+    open('site/index.html').write(template.render(steps=raw_steps))
+    
+def genSiteDaemon():
+    # Can't use firebase.on() so we have to just pull every PULL_RATE
+    # ms
+    while True:
+        print "Pulling..."
+        raw = pullFromServer()
+        generateSite(raw)
+        time.sleep(PULL_RATE)
 
 def block(steps):
     thresholds = [
@@ -18,5 +54,4 @@ def block(steps):
 def reloadCache():
     (local["dscacheutil"]["-flushcache"])()
 if __name__ == "__main__":
-    resetHost()
-    block(1000)
+    genSiteDaemon()
